4546deb2cc6fd14abeb22da7d369fe9729d761ea
adruidiaz <adruidiaz@unicesar.edu.co>
2026-03-17 09:27:19 -0500

    creacion de clase vehiculo con sus atributos y metodos

--- SistemaDeTickets/src/model/Vehiculo.java
+++ SistemaDeTickets/src/model/Vehiculo.java
@@ -8,6 +8,74 @@ package model;
  *
  * @author hp
  */
-public class Vehiculo {
-    
+public abstract class Vehiculo {
+ public String  placa; 
+ public String  ruta; 
+ public int  capacidadMaxima; 
+ public int  pasajerosActuales;
+ public double  tarifaBase;
+ public boolean  disponible;
+
+    public Vehiculo(String placa, String ruta, int capacidadMaxima, int pasajerosActuales, double tarifaBase, boolean disponible) {
+        this.placa = placa;
+        this.ruta = ruta;
+        this.capacidadMaxima = capacidadMaxima;
+        this.pasajerosActuales = pasajerosActuales;
+        this.tarifaBase = tarifaBase;
+        this.disponible = disponible;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+
+    public int getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    public void setCapacidadMaxima(int capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+    }
+
+    public int getPasajerosActuales() {
+        return pasajerosActuales;
+    }
+
+    public void setPasajerosActuales(int pasajerosActuales) {
+        this.pasajerosActuales = pasajerosActuales;
+    }
+
+    public double getTarifaBase() {
+        return tarifaBase;
+    }
+
+    public void setTarifaBase(double tarifaBase) {
+        this.tarifaBase = tarifaBase;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+ 
+ 
+ 
+ 
+ 
+ 
 }
