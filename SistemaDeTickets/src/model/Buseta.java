f6e67a2bc7508cc0a2b72cdecf3cfb2fb655194b
adruidiaz <adruidiaz@unicesar.edu.co>
2026-03-17 10:18:58 -0500

    creacion de la subclase Buseta

---
+++ SistemaDeTickets/src/model/Buseta.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author hp
+ */
+public class Buseta extends Vehiculo {
+    
+    public Buseta(String placa, String ruta, int capacidadMaxima, int pasajerosActuales, double tarifaBase, boolean disponible) {
+        super(placa, ruta, capacidadMaxima, pasajerosActuales, tarifaBase, disponible);
+    }
+    
+}
