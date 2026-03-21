c74815292d84fa3205d8fbb905aeef5eaad12ef2
adruidiaz <adruidiaz@unicesar.edu.co>
2026-03-21 11:02:07 -0500

    feat: creacion de la clase ReservaService para confirmar reserva

---
+++ SistemaDeTickets/src/Service/ReservaService.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Service;
+
+import DAO.ReservaDAO;
+import dao.TicketDAO;
+import java.io.IOException;
+import java.time.LocalDate;
+import model.Reserva;
+import model.Ticket;
+
+/**
+ *
+ * @author hp
+ */
+public class ReservaService {
+    private ReservaDAO reservaDAO;
+    private TicketDAO ticketDAO;
+
+    public ReservaService(ReservaDAO reservaDAO, TicketDAO ticketDAO) {
+        this.reservaDAO = reservaDAO;
+        this.ticketDAO = ticketDAO;
+    }
+
+    public void confirmarReserva(Reserva r) throws IOException {
+        // Cambiar estado a CONVERTIDA
+        reservaDAO.actualizarEstado(r.getCodigo(), "CONVERTIDA");
+
+        // Crear ticket y guardarlo en tickets.txt
+        Ticket t = new Ticket(r.getPasajero(), r.getVehiculo(), LocalDate.now(),
+                              r.getRuta().getOrigen(), r.getRuta().getDestino(), 0);
+        ticketDAO.guardarTicket(t);
+    }
+
+    public void cancelarReserva(String codigoReserva) throws IOException {
+        reservaDAO.actualizarEstado(codigoReserva, "CANCELADA");
+    }
+}
